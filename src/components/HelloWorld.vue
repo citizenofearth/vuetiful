f36dca63bb4a237f5aead8eb32f65f9e919956cd
Tenzin Namgyal <tenzin.namgyal@mindgeek.com>
2019-04-26 09:57:46 -0400

    Add coindesk API

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,14 +27,34 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <div>
+      {{ info }}
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: function () {
+    return {
+      info: ""
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => {
+        this.info = response
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
 }
 </script>
