de644914ea39c6b9c36f179241f3fb96e40332f5
Tenzin Namgyal <tenzin.namgyal@mindgeek.com>
2019-04-26 16:45:08 -0400

    Add bitcoin price index

---
+++ src/components/BPI.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="bpi">
+    <h2>Bitcoin Price Index</h2>
+
+    <div
+      v-for="currency in info"
+      :key="currency"
+      class="currency">
+    {{ currency.description }}:
+      <span class="lighten">
+        <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'BPI',
+  props: {
+    msg: String
+  },
+  data: function () {
+    return {
+      info: null,
+      loading: true,
+      errored: false
+    }
+  },
+  filters: {
+    currencydecimal (value) {
+      return value.toFixed(2)
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => {
+        this.info = response.data.bpi
+      })
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: $primary-light;
+}
+</style>
